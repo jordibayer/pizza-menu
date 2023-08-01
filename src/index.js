25df88e72af664137974cabc14a2a51d14457443
Jordi <jordi.be.1992@gmail.com>
2023-08-01 17:58:46 +0200

    ternay operators on pizza component

--- src/index.js
+++ src/index.js
@@ -89,6 +89,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img
@@ -110,10 +112,17 @@ function Footer() {
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
+  // if (!isOpen)
+  //   return (
+  //     <p>
+  //       We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+  //     </p>
+  //   );
+
   return (
     <footer className="footer">
       {isOpen ? (
-        <div>
+        <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
