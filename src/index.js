5eb9f74f66e2e87d4722aa1f211e2323b02a9989
Jordi <jordi.be.1992@gmail.com>
2023-08-01 18:06:53 +0200

    created new component

--- src/index.js
+++ src/index.js
@@ -106,6 +106,15 @@ function Pizza(props) {
   );
 }
 
+function Order(props) {
+  return (
+    <div className="order">
+      <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 20;
@@ -122,10 +131,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
