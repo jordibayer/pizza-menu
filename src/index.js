00fef6dd4cab323e32c2ab4ebeb6b1200320696e
Jordi <jordi.be.1992@gmail.com>
2023-08-01 18:22:39 +0200

    .

--- src/index.js
+++ src/index.js
@@ -88,28 +88,28 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObj.soldOut) return null;
+function Pizza(pizzaObj) {
+  if (pizzaObj.soldOut) return null;
 
   return (
     <li className="pizza">
       <img
-        src={props.pizzaObj.photoName}
-        alt={props.pizzaObj.name}
+        src={pizzaObj.photoName}
+        alt={pizzaObj.name}
       />
       <div>
-        <h3>{props.pizzaObj.name}</h3>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}€</span>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.price}€</span>
       </div>
     </li>
   );
 }
 
-function Order(props) {
+function Order(closeHour) {
   return (
     <div className="order">
-      <p>We're open until {props.closeHour}:00. Come visit us or order online.</p>
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
       <button className="btn">Order</button>
     </div>
   );
