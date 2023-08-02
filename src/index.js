7ba0188d4244adeb7dd6cb37f8971b0d71aff27b
Jordi <jordi.be.1992@gmail.com>
2023-08-02 16:57:48 +0200

    feat: conditional css added

--- src/index.js
+++ src/index.js
@@ -69,18 +69,22 @@ function Header() {
 function Menu() {
   const pizzas = pizzaData;
   const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza
-              pizzaObj={pizza}
-              key={pizza.name}
-            />
-          ))}
-        </ul>
+        <>
+          <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza
+                pizzaObj={pizza}
+                key={pizza.name}
+              />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
@@ -88,11 +92,11 @@ function Menu() {
   );
 }
 
-function Pizza(pizzaObj) {
-  if (pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img
         src={pizzaObj.photoName}
         alt={pizzaObj.name}
@@ -100,13 +104,13 @@ function Pizza(pizzaObj) {
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}€</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
 }
 
-function Order(closeHour) {
+function Order({ closeHour }) {
   return (
     <div className="order">
       <p>We're open until {closeHour}:00. Come visit us or order online.</p>
